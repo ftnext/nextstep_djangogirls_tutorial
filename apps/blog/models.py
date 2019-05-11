a1d91b68457fc7b894a26b702da51a06e19ba799
ftnext <takuyafjp+develop@gmail.com>
2019-05-11 11:16:48 +0900

    [feat] 「Djangoモデル」のブログポストモデルをアップデートして作成
    * verbose_nameを指定（フォームのフィールド名に使われる） ref: https://docs.djangoproject.com/en/2.2/topics/db/models/#verbose-field-names
    * auth.UserモデルへのForeignKeyを定数を使った書き方に変更 ref: https://www.slideshare.net/tokibito/django-79549364 (28スライド)
    * Metaクラスにorderingで記事の順番を指定した（公開されている記事は公開日が新しいものが先、公開されていない場合は作成日が新しいものが先）

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+ 
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        verbose_name='作成者'
+    )
+    title = models.CharField('タイトル', max_length=200)
+    text = models.TextField('本文')
+    created_date = models.DateTimeField('作成日', default=timezone.now)
+    published_date = models.DateTimeField('公開日', blank=True, null=True)
+
+    class Meta:
+        ordering = ['-published_date', '-created_date']
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
