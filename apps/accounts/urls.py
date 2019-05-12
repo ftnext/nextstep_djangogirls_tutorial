780444a601b7441be16dee90a2fa0258beaa8295
ftnext <takuyafjp+develop@gmail.com>
2019-05-12 14:28:49 +0900

    [feat] ログアウト機能を追加し、page-headerからログイン／ログアウトできるように変更（（「宿題: ウェブサイトをセキュアにする」））
    * blog.cssを修正し、Logoutのリンクの文字の色を他の項目と揃えた
    * シークレットブラウザで起動した際、Log outのリンクが白い文字でなかったため、文字を白くするスタイル指定を追加した

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -5,4 +5,7 @@ from django.urls import path
 app_name = 'accounts'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/',
+         auth_views.LogoutView.as_view(next_page='blog:post_list'),
+         name='logout'),
 ]
